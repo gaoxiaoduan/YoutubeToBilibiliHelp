6d76adf29e720baf5ee82f4fa6d7230b50779477
coderduan <1961018000@qq.com>
2023-12-12 14:45:54 +0800

    ci(uploadThumbnail): 更新上传封面时操作元素的类名

--- src/browser/blibli/uploadThumbnail.ts
+++ src/browser/blibli/uploadThumbnail.ts
@@ -11,15 +11,15 @@ export const uploadThumbnail = async (page: Page, changedInfo: IChangedInfo) => 
     if (!fs.existsSync(outputThumbnail)) return true;
 
     await delay(1000 * 15);
-    await page.click(".cover-upload-btn span:first-child");
+    await page.click(".cover-upload-mask-btn span:first-child");
     await delay(1000 * 5);
-    await page.click(".bcc-dialog .cover-cut-header-tab-item:last-child > .text");
+    await page.click(".bcc-dialog .cover-select-header-tab-item:last-child > .text");
 
 
     logger.info("开始选择上传封面文件");
     const [fileChooser] = await Promise.all([
         page.waitForFileChooser(),
-        page.click(".cover-cut-footer-upload .bcc-upload .bcc-button"), // 上传文件按钮
+        page.click(".bcc-upload .bcc-button"), // 上传文件按钮
     ]);
 
     // 选择要上传的文件
@@ -37,7 +37,7 @@ export const uploadThumbnail = async (page: Page, changedInfo: IChangedInfo) => 
         logger.info("图片解析成功");
     }
 
-    await page.click(".cover-cut-footer .bcc-button--primary");
+    await page.click(".cover-select-footer-pick .bcc-button--primary");
     logger.info("封面上传成功", outputThumbnail);
     return true;
 };
